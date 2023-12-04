4752c69063a7435a23b896f314c1907530338eeb
igotchu730 <145312829+igotchu730@users.noreply.github.com>
2023-12-03 18:15:07 -0800

    DB password change
    Co-Authored-By: mo-i-khan <145402375+mo-i-khan@users.noreply.github.com>

--- src/main/java/DatabaseManager.java
+++ src/main/java/DatabaseManager.java
@@ -16,7 +16,7 @@ public class DatabaseManager {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl("jdbc:mysql://project380hotel.chazaxvumqbx.us-west-1.rds.amazonaws.com:3306/main");
         config.setUsername("hotelManager_7");
-        config.setPassword("cr4#Ha5PiFrU8ezlcRoD");
+        config.setPassword("xEp+U=poTus=lsTib9l&");
 
         dataSource = new HikariDataSource(config);
     }
